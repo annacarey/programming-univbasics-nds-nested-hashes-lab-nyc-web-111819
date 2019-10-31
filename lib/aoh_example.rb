15b18b3a65764eef558d675f81096685939ab072
declarative-blog-8005 <anna@annajcarey.com>
2019-10-31 21:12:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  new_array = [DON_G]
+  new_array.push
 end
 
 def aoh_lookup(aoh, row, key)
